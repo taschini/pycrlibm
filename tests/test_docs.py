ca19d54c77248c12f4040e3c0dadeec222c8c812
taschini <taschini@users.noreply.github.com>
2015-10-27 10:02:54 +0100

    Added tests to verify the code examples on all rst files.

---
+++ tests/test_docs.py
@@ -0,0 +1,31 @@
+"""
+test_docs
+===========
+
+Verify the code examples on all reStructuredText files in the project.
+
+"""
+
+def rst_files():
+    import os, os.path as path
+    root = path.abspath(path.join(path.dirname(__file__), '..'))
+    def prune(p, d):
+        return d.startswith('.') or (p == root and d == 'build')
+    def filter(p, f):
+        return f.endswith('.rst')
+    for (dirpath, dirnames, filenames) in os.walk(root):
+        dirnames[:] = [d for d in dirnames if not prune(dirpath, d)]
+        filenames = [f for f in filenames if filter(dirpath, f)]
+        for f in filenames:
+            yield path.join(dirpath, f)
+
+
+def load_tests(loader, tests, pattern):
+    # The load_tests protocol has been introduced in Python 2.7, so
+    # doctests will be skipped on earlier Pythons.  This is an
+    # advantage in this case, as changes in float.__repr__ would break
+    # doctests anyway.
+    import unittest, doctest
+    s = unittest.TestSuite()
+    s.addTest(doctest.DocFileSuite(module_relative=False, *rst_files()))
+    return s
